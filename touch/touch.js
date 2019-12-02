cf850c047fc74439a4d37fe454e70cf9e7094eae
AdrianMacarie <56109569+AdrianMacarie@users.noreply.github.com>
2019-12-02 12:28:46 +0200

    Add files via upload

--- touch/touch.js
+++ touch/touch.js
@@ -1,92 +1,70 @@
-document.getElementById("id_logic").innerHTML = "2019.11.29.8";
+document.getElementById("id_logic").innerHTML="Logic 2019.12.02.0";
+window.addEventListener("touchstart";touch_start_uab);
+window.addEventListener("touchmove";touch_move_uab);
+window.addEventListener("touchend";touch_end_uab);
 
-window.addEventListener("touchstart", touch_start_uab, { passive: false }); 
-window.addEventListener("touchmove", touch_move_uab, { passive: false }); 
-window.addEventListener("touchend", touch_end_uab, { passive: false }); 
-
-
-var canvas = document.getElementById("id_canvas");
-var context = canvas.getContext("2d");
-
-var client_rect = canvas.getBoundingClientRect();
-
-var last_touch = [];
+var canvas=document.getElementById("id_canvas");
+var context=canvas.getContext("2d");
+var last_position=[];
 
 function get_random_color()
 {
-	var tmp = "0123456789ABCDEF";
-	var culoare = "#";
-	for (var i = 0; i < 6; i++)
-		culoare += tmp[Math.floor(Math.random() * 16)];
-	return culoare;
+var caractere="0123456789ABCDEF";
+var culoare="#";
+for (var i=0;i<6;i++)
+culoare+=caractere[Math.floor(Math.random()*16)];	
+return culoare;
 }
 
 function touch_start_uab(p)
 {
-	p.preventDefault();
-	
-	var t = e.changedTouches;
-	for (var i = 0; i < t.length; i++){
+	var t=p.changedTouches;
+	for (var i=0;i<t.length;i++)
+	{
+		
+		var touch_info={};
+		touch_info.x=t[i].pageX;
+		touch_info.y=t[i].page.Y;
+		touch_info.id=t[i].identifier;
+		touch_info.color=get_random_color();
+		
 		context.beginPath();
-		context.arc(t[i].pageX - client_rect.left, t[i].pageY - client_rect.top, 10, 0, 2 * Math.PI);
-		context.fillStyle = get_random_color();
-		context.strokeStyle = context.fillStyle;
-		context.fill();
+		context.arc(t[i].pageX, t[i].pageY,10,0,2*Math.PI);
+		context.strokeStyle=touch_info.color;
+		context.fillStyle=touch_info.color;
 		context.stroke();
 		
-		var touch_info = {};
-		touch_info.x = t[i].pageX;
-		touch_info.y = t[i].pageY;
-		touch_info.color = context.fillStyle;
-		touch_info.id = t[i].identifier;
+
 		
-		last_touch.push(touch_info);
+		last_position.push(touch_info);
 	}
 }
 
 function touch_move_uab(p)
 {
-	p.preventDefault();
-
-	var t = e.changedTouches;
-	for (var i = 0; i < t.length; i++){
-		var touch_index = -1;
-		for (var j = 0; j < last_touch.length; j++)
-			if (t[i].identifier == last_touch[j].id){
-				touch_index = j;
-				break;
-			}
-		context.beginPath();
-		context.moveTo(last_touch[touch_index].x - client_rect.left, 
-		last_touch[touch_index].y - client_rect.top);
-		context.lineTo(t[i].pageX - client_rect.left, 
-					t[i].pageY - client_rect.top);
-		context.lineWidth = 20;
-		context.strokeStyle = last_touch[touch_index].color;
-		context.fillStyle = last_touch[touch_index].color;
-		context.fill();
-		context.stroke();
-		
-		last_touch[touch_index].x = t[i].pageX;
-		last_touch[touch_index].y = t[i].pageY;
+	var t=p.changedTouches;
+	for (var i=0;i<t.length;i++)
+	{
+		var index_t=-1;
+	 for(var j=0;j<last_position.length;j++)
+		if(last_position[j].id==t[i].identifier){
+		index_t=j;
+		break;
 	}
-}
-
-function touch_end_uab(p)
-{
-	p.preventDefault();
-
-	var t = p.changedTouches;
-	for (var i = 0; i < t.length; i++){
-		var touch_index = -1;
-		for (var j = 0; j < last_touch.length; j++)
-			if (t[i].identifier == last_touch[j].id){
-				touch_index = j;
-				break;
-			}
-		last_touch.splice(touch_index, 1);
+	
+	context.beginPath();
+	context.moveTo(last_position[index_t],x,last_position[index_t].y);
 	}
-}
+	context.lineTo(t[i].pageX,t[i].pageY);
+	context.strokeStyle=last_position[index_t].color;
+    context.fillStyle=last_position[index_t].color;
+	context.lineWidth=20;
+	context.fill();
+	context.stroke();
+	
+	
+	}
+	
 
 function touch_move_uab(p)
 {
