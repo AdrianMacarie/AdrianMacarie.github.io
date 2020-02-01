86a44aa3a83e9c5691686a7b3d4133d019152c50
adrianmacarie <56109569+AdrianMacarie@users.noreply.github.com>
2020-02-01 23:39:45 +0200

    +

--- aplicatiecamera/cam.js
+++ aplicatiecamera/cam.js
@@ -15,3 +15,25 @@ function on_fail_cam_uab(e)
 {
 	alert("Esec " + e);
 }
+
+var options = {
+  mode: 'picture',
+  recorderProfile: 'jpg',
+  previewSize: {
+    width: 352,
+    height: 288
+  }
+};
+
+var camera = navigator.mozCameras.getListOfCameras()[0];
+
+function onSuccess(cameraObj) {
+  var cameraControl = cameraObj.camera;
+  // Do stuff with the cameraControl
+};
+
+function onError(error) {
+  console.warn(error);
+};
+
+navigator.mozCameras.getCamera(camera, options).then(onSuccess, onError);
