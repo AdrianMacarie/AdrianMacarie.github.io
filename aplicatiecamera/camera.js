1ac2b68a4acc2d60bc5b618551bf2d232777b94d
adrianmacarie <56109569+AdrianMacarie@users.noreply.github.com>
2020-02-02 01:30:22 +0200

    +

--- aplicatiecamera/camera.js
+++ aplicatiecamera/camera.js
@@ -1,51 +1,19 @@
-var video = document.getElementById("id_video");
 
-var c = { audio: true, video: { facingMode: "environment" }};
+navigator.mozCameras.getCamera(camera, options).then(onSuccess, onError);
 
-navigator.mediaDevices.getUserMedia(c).then(on_ok_cam_uab).catch(on_fail_cam_uab);
 
-function on_ok_cam_uab(e)
-{
-	video.srcObject = e;
-}
-
-function on_fail_cam_uab(e)
-{
-	alert("Esec " + e);
-}
+var sizes = instanceOfCameraControl.capabilities.pictureSizes
 
 var options = {
   camera: navigator.mozCameras.getListOfCameras()[0]
 };
  
-function onSuccess( camera ) {
-  var capabilities = camera.camera.capabilities;
-
-  if (capabilities.sceneModes.indexOf('night') > -1) {
-    camera.sceneMode = 'night';
-  }
-};
-
-navigator.mozCameras.getCamera(options).then(onSuccess)
-
-
-var options = {
-  mode: 'picture',
-  recorderProfile: 'jpg',
-  previewSize: {
-    width: 352,
-    height: 288
-  }
-};
-
-var camera = navigator.mozCameras.getListOfCameras()[0];
-
 function onSuccess(camera) {
-  // Do stuff with the camera
+  var sizes = camera.camera.capabilities.pictureSizes;
+  
+  sizes.forEach(function (size) {
+    console.log(size.width + 'x' + size.height);
+  });
 };
 
-function onError(error) {
-  console.warn(error);
-};
-
-navigator.mozCameras.getCamera(camera, options, onSuccess, onError);
+navigator.mozCameras.getCamera(options).then(onSuccess);
