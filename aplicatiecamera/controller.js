fb295e8e31dbca21564988d0f79c9d6b7a825bcc
adrianmacarie <56109569+AdrianMacarie@users.noreply.github.com>
2020-02-02 01:08:53 +0200

    +

--- aplicatiecamera/controller.js
+++ aplicatiecamera/controller.js
@@ -1,33 +1,21 @@
-CameraControl.resumeRecording();
-var storage = navigator.getDeviceStorage('videos');
-var cameraOptions = {
-  camera: navigator.mozCameras.getListOfCameras()[0]
-};
-var recordOptions = {
-  rotation: 0;
-  maxFileSizeBytes: 1024 * 1024 * 1024; // 1 GB
-  maxVideoLengthMs: 1000 * 60 * 60;     // 1 hour
-}
-
-function onRecordStart() {
-  console.log("The device is recording the video output from the camera");
-}
-
-function onStateChange( newState ) {
-  console.log("The recorder state change: " + newState);
-}
-
-function onAccessCamera( camera ) {
-  camera.startRecording(recordOptions, storage, 'myVideo.mpg', onRecordStart);
-  camera.onRecorderStateChange = onStateChange;
-
-  pauseBtn.onclick = function() {
-    camera.pauseRecording();
-  }
-
-  resumeBtn.onclick = function() {
-    camera.resumeRecording();
+var options = {
+  mode: 'picture',
+  recorderProfile: 'jpg',
+  previewSize: {
+    width: 352,
+    height: 288
   }
 };
 
-navigator.mozCameras.getCamera(cameraOptions, onAccessCamera)
+var camera = navigator.mozCameras.getListOfCameras()[0];
+
+function onSuccess(cameraObj) {
+  var cameraControl = cameraObj.camera;
+  // Do stuff with the cameraControl
+};
+
+function onError(error) {
+  console.warn(error);
+};
+
+navigator.mozCameras.getCamera(camera, options).then(onSuccess, onError);
