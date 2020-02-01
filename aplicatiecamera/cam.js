56a75d6f8b266a67a814819d8569345f53bed59c
adrianmacarie <56109569+AdrianMacarie@users.noreply.github.com>
2020-02-01 23:46:28 +0200

    +

--- aplicatiecamera/cam.js
+++ aplicatiecamera/cam.js
@@ -25,6 +25,7 @@ var options = {
   }
 };
 
+
 var camera = navigator.mozCameras.getListOfCameras()[0];
 
 function onSuccess(cameraObj) {
@@ -37,3 +38,18 @@ function onError(error) {
 };
 
 navigator.mozCameras.getCamera(camera, options).then(onSuccess, onError);
+
+var sizes = instanceOfCameraControl.capabilities.pictureSizes
+var options = {
+  camera: navigator.mozCameras.getListOfCameras()[0]
+};
+ 
+function onSuccess(camera) {
+  var sizes = camera.camera.capabilities.pictureSizes;
+  
+  sizes.forEach(function (size) {
+    console.log(size.width + 'x' + size.height);
+  });
+};
+
+navigator.mozCameras.getCamera(options).then(onSuccess);
