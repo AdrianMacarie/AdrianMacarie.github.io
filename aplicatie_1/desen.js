51e4e1658ef1d4a0a987ac38fab68e2fde13b425
adrianmacarie <56109569+AdrianMacarie@users.noreply.github.com>
2020-02-01 19:02:52 +0200

    ++

--- aplicatie_1/desen.js
+++ aplicatie_1/desen.js
@@ -1,5 +1,8 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-
 ctx.fillStyle = 'green';
 ctx.fillRect(10, 10, 150, 100);
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(0, 0, 150, 75);
