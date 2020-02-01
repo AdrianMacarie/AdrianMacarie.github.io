e348e25b7cd90c5df197ba1643491d98f5d186ad
adrianmacarie <56109569+AdrianMacarie@users.noreply.github.com>
2020-02-01 23:48:54 +0200

    +

--- aplicatiecamera/cam.js
+++ aplicatiecamera/cam.js
@@ -1,31 +1,3 @@
-document.getElementById("id_logic_version").innerHTML = "2019.11.22.1";
-
-var video = document.getElementById("id_video");
-
-var c = { audio: true, video: { facingMode: "environment" }};
-
-navigator.mediaDevices.getUserMedia(c).then(on_ok_cam_uab).catch(on_fail_cam_uab);
-
-function on_ok_cam_uab(e)
-{
-	video.srcObject = e;
-}
-
-function on_fail_cam_uab(e)
-{
-	alert("Esec " + e);
-}
-
-var options = {
-  mode: 'picture',
-  recorderProfile: 'jpg',
-  previewSize: {
-    width: 352,
-    height: 288
-  }
-};
-
-
 var camera = navigator.mozCameras.getListOfCameras()[0];
 
 function onSuccess(cameraObj) {
