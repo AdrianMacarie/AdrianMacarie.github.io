e7a88e2d083ea09394c1a02f76ccb5319a583f46
adrianmacarie <56109569+AdrianMacarie@users.noreply.github.com>
2020-02-02 00:14:28 +0200

    +

--- aplicatiecamera/camera.js
+++ aplicatiecamera/camera.js
@@ -15,3 +15,16 @@ function on_fail_cam_uab(e)
 {
 	alert("Esec " + e);
 }
+var options = {
+  camera: navigator.mozCameras.getListOfCameras()[0]
+};
+ 
+function onSuccess( camera ) {
+  var capabilities = camera.camera.capabilities;
+
+  if (capabilities.sceneModes.indexOf('night') > -1) {
+    camera.sceneMode = 'night';
+  }
+};
+
+navigator.mozCameras.getCamera(options).then(onSuccess)
