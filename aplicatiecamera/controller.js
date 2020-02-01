3c409efc77cfe3f3588546964fba183fa2ce7960
adrianmacarie <56109569+AdrianMacarie@users.noreply.github.com>
2020-02-02 00:34:35 +0200

    +

--- aplicatiecamera/controller.js
+++ aplicatiecamera/controller.js
@@ -1,5 +1,5 @@
 CameraControl.resumeRecording();
-var storage = navigator.getDeviceStorage('id_videos');
+var storage = navigator.getDeviceStorage('id_video');
 var cameraOptions = {
   camera: navigator.mozCameras.getListOfCameras()[0]
 };
