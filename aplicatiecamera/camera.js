719963f8ddcda9b5ff9e7049531967b5074e7bc7
adrianmacarie <56109569+AdrianMacarie@users.noreply.github.com>
2020-02-01 23:59:32 +0200

    +

--- aplicatiecamera/camera.js
+++ aplicatiecamera/camera.js
@@ -1,14 +1,3 @@
-var camera = navigator.mozCameras.getListOfCameras()[0];
-
-function onSuccess(cameraObj) {
-  var cameraControl = cameraObj.camera;
-  // Do stuff with the cameraControl
-};
-
-function onError(error) {
-  console.warn(error);
-};
-
 navigator.mozCameras.getCamera(camera, options).then(onSuccess, onError);
 
 
@@ -29,11 +18,3 @@ function onSuccess(camera) {
 navigator.mozCameras.getCamera(options).then(onSuccess);
 
 
-var imageCapture = new ImageCapture(videoTrack)
- navigator.mediaDevices.getUserMedia({video: true})
-.then(mediaStream => { 
-  document.querySelector('video').srcObject = mediaStream
-  const track = mediaStream.getVideoTracks()[0];
-  imageCapture = new ImageCapture(track);
-})
-.catch(error => console.log(error));
