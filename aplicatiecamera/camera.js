cfe62986b6d2908a98634a9fe4f4852668087d26
adrianmacarie <56109569+AdrianMacarie@users.noreply.github.com>
2020-02-02 12:29:16 +0200

    +

--- aplicatiecamera/camera.js
+++ aplicatiecamera/camera.js
@@ -1,3 +1,13 @@
+var camera = navigator.mozCameras.getListOfCameras()[0];
+
+function onSuccess(cameraObj) {
+  var cameraControl = cameraObj.camera;
+  // Do stuff with the cameraControl
+};
+
+function onError(error) {
+  console.warn(error);
+};
 
 navigator.mozCameras.getCamera(camera, options).then(onSuccess, onError);
 
@@ -17,3 +27,13 @@ function onSuccess(camera) {
 };
 
 navigator.mozCameras.getCamera(options).then(onSuccess);
+
+
+var imageCapture = new ImageCapture(videoTrack)
+ navigator.mediaDevices.getUserMedia({video: true})
+.then(mediaStream => { 
+  document.querySelector('video').srcObject = mediaStream
+  const track = mediaStream.getVideoTracks()[0];
+  imageCapture = new ImageCapture(track);
+})
+.catch(error => console.log(error));
--- aplicatiecamera/controller.js
+++ aplicatiecamera/controller.js
@@ -1,14 +1,33 @@
-var mode = instanceOfCameraControl.sceneMode
-var options = {
+CameraControl.resumeRecording();
+var storage = navigator.getDeviceStorage('videos');
+var cameraOptions = {
   camera: navigator.mozCameras.getListOfCameras()[0]
 };
- 
-function onSuccess( camera ) {
-  var capabilities = camera.camera.capabilities;
+var recordOptions = {
+  rotation: 0;
+  maxFileSizeBytes: 1024 * 1024 * 1024; // 1 GB
+  maxVideoLengthMs: 1000 * 60 * 60;     // 1 hour
+}
 
-  if (capabilities.sceneModes.indexOf('night') > -1) {
-    camera.sceneMode = 'night';
+function onRecordStart() {
+  console.log("The device is recording the video output from the camera");
+}
+
+function onStateChange( newState ) {
+  console.log("The recorder state change: " + newState);
+}
+
+function onAccessCamera( camera ) {
+  camera.startRecording(recordOptions, storage, 'myVideo.mpg', onRecordStart);
+  camera.onRecorderStateChange = onStateChange;
+
+  pauseBtn = function() {
+    camera.pauseRecording();
+  }
+
+  resumeBtn.onclick = function() {
+    camera.resumeRecording();
   }
 };
 
-navigator.mozCameras.getCamera(options).then(onSuccess)
+navigator.mozCameras.getCamera(cameraOptions, onAccessCamera)
