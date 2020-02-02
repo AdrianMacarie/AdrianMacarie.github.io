f7ebf44b7ba60d86b2f62ff379314af12b7aad4a
AdrianMacarie <56109569+AdrianMacarie@users.noreply.github.com>
2020-02-02 14:26:40 +0200

    Add files via upload

---
+++ aplicatieexamen/camera.js
@@ -0,0 +1,15 @@
+var video = document.getElementById("id_video");
+
+var c = {video: { facingMode: "environment" }};
+
+navigator.mediaDevices.getUserMedia(c).then(on_ok_cam_uab).catch(on_fail_cam_uab);
+
+function on_ok_cam_uab(e)
+{
+	video.srcObject = e;
+}
+
+function on_fail_cam_uab(e)
+{
+	alert("Esec " + e);
+}
